4ae5bb24f4b6d7d174d66f65c4ea4353ee3fc263
gglue <generalglue@gmail.com>
2022-10-29 05:49:56 -0400

    Started create-react-app

--- src/main/java/noodleshop/data/UserRepository.java
+++ src/main/java/noodleshop/data/UserRepository.java
@@ -2,8 +2,6 @@ package noodleshop.data;
 
 import noodleshop.NoodleUser;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.security.access.prepost.PreAuthorize;
-
 public interface UserRepository extends CrudRepository<NoodleUser, Long> {
 
     NoodleUser findByUsername(String username);
